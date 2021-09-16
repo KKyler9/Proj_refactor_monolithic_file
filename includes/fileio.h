ea244eff354dafdef18e2ed96b4ce59ab245b527
KKyler9 <kyle.williams.19@cnu.edu>
2021-09-16 17:52:57 -0400

    Intial File setup, still need to link and compile

---
+++ includes/fileio.h
@@ -0,0 +1,5 @@
+#include <string>
+#include <vector>
+#include <iostream>
+int load(const std::string filename, std::vector<process> &myProcesses);
+int save(const std::string filename, std::vector<process> &myProcesses)
